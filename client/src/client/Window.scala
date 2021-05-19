33173dd79842099e5e8b1acc54864f0f0af00075
Ramon Brand <ramon2009@gmail.com>
2021-05-19 22:05:43 +0000

    Add texture ability to batch renderer

--- client/src/client/Window.scala
+++ client/src/client/Window.scala
@@ -80,11 +80,14 @@ object Window {
     glEnable(GL_CULL_FACE)
     glCullFace(GL_BACK)
     glFrontFace(GL_CCW)
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
     // Test
     class Sprite(var x: Float = 0, var y: Float = 0) {
       def getPositions(): Array[Float] = Sprite.positions
       def getColors(): Array[Float] = Sprite.colors
+      def getUvs(): Array[Float] = Sprite.uvs
       def getIndexes(): Array[Int] = Sprite.indexes
     }
     object Sprite {
@@ -100,6 +103,12 @@ object Window {
         0.5f, 0.5f, 0.8f,
         0.5f, 0.0f, 0.5f,
       )
+      val uvs = Array(
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+      )
       val indexes = Array[Int](
         0, 1, 2, 2, 3, 0
       )
@@ -109,6 +118,7 @@ object Window {
       private var spriteCount = 0
       val positions = new ArrayBuffer[Float]()
       val colors = new ArrayBuffer[Float]()
+      val uvs = new ArrayBuffer[Float]()
       val indexes = new ArrayBuffer[Int]()
       def addSprite(sprite: Sprite) = {
         positions.addAll(sprite.getPositions())
@@ -121,26 +131,46 @@ object Window {
           }
         }
         colors.addAll(sprite.getColors())
+        uvs.addAll(sprite.getUvs())
         indexes.addAll(sprite.getIndexes().map(i => i + spriteCount * 4))
         spriteCount += 1
       }
+      val textureHandle = glGenTextures()
       val vboPositions = glGenBuffers()
       val vboColors = glGenBuffers()
+      val vboUvs = glGenBuffers()
       val vio = glGenBuffers()
       val vao = glGenVertexArrays()
       val vertexShader = glCreateShader(GL_VERTEX_SHADER)
       val fragmentShader = glCreateShader(GL_FRAGMENT_SHADER)
       val shaderProgram = glCreateProgram()
       var uniformGlobalColor = 0
+      var uniformMainTexture = 1
       private def buildShader() = {
+        glBindTexture(GL_TEXTURE_2D, textureHandle)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 2, 2, 0, GL_RGBA, GL_FLOAT, Array(
+          1.0f, 0.0f, 0.0f, 0.5f,
+          1.0f, 1.0f, 1.0f, 0.5f,
+          1.0f, 1.0f, 1.0f, 0.5f,
+          1.0f, 1.0f, 1.0f, 0.9f,
+        ))
+        glGenerateMipmap(GL_TEXTURE_2D)
+
         val vertexShaderSource =
           """
             |#version 400
             |in vec2 vertexPosition;
             |in vec3 vertexColor;
+            |in vec2 vertexUvs;
             |out vec3 color;
+            |out vec2 uvs;
             |void main(void) {
             | color = vertexColor;
+            | uvs = vertexUvs;
             | gl_Position = vec4(vertexPosition, 1.0, 1.0);
             |}
             |""".stripMargin
@@ -149,10 +179,13 @@ object Window {
           """
             |#version 400
             |uniform vec4 globalColor;
+            |uniform sampler2D mainTexture;
             |in vec3 color;
+            |in vec2 uvs;
             |out vec4 frag_colour;
             |void main(void) {
-            | frag_colour = vec4(globalColor.xyz * color, 1.0);
+            | //frag_colour = vec4(globalColor.xyz * color, 1.0);
+            | frag_colour = texture(mainTexture, uvs);
             |}
             |""".stripMargin
 
@@ -170,12 +203,15 @@ object Window {
         Validate.program(shaderProgram)
 
         uniformGlobalColor = glGetUniformLocation(shaderProgram, "globalColor")
+        uniformMainTexture = glGetUniformLocation(shaderProgram, "mainTexture")
       }
       def buildBuffers() = {
         glBindBuffer(GL_ARRAY_BUFFER, vboPositions)
         glBufferData(GL_ARRAY_BUFFER, positions.length * 4, GL_STATIC_DRAW)
         glBindBuffer(GL_ARRAY_BUFFER, vboColors)
         glBufferData(GL_ARRAY_BUFFER, colors.length * 4, GL_STATIC_DRAW)
+        glBindBuffer(GL_ARRAY_BUFFER, vboUvs)
+        glBufferData(GL_ARRAY_BUFFER, uvs.length * 4, GL_STATIC_DRAW)
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vio)
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexes.length * 4, GL_STATIC_DRAW)
         
@@ -184,8 +220,11 @@ object Window {
         glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, NULL)
         glBindBuffer(GL_ARRAY_BUFFER, vboColors)
         glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, NULL)
+        glBindBuffer(GL_ARRAY_BUFFER, vboUvs)
+        glVertexAttribPointer(2, 2, GL_FLOAT, false, 0, NULL)
         glEnableVertexAttribArray(0)
         glEnableVertexAttribArray(1)
+        glEnableVertexAttribArray(2)
       }
       def build() = {
         buildShader()
@@ -196,12 +235,15 @@ object Window {
         glBufferSubData(GL_ARRAY_BUFFER, 0, positions.toArray)
         glBindBuffer(GL_ARRAY_BUFFER, vboColors)
         glBufferSubData(GL_ARRAY_BUFFER, 0, colors.toArray)
+        glBindBuffer(GL_ARRAY_BUFFER, vboUvs)
+        glBufferSubData(GL_ARRAY_BUFFER, 0, uvs.toArray)
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vio)
         glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, indexes.toArray)
       }
       def render() = {
         glUseProgram(shaderProgram)
         glUniform4f(uniformGlobalColor, 0.0f, 1.0f, 1.0f, 1.0f)
+        glBindTexture(GL_TEXTURE_2D, textureHandle)
         glBindVertexArray(vao)
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vio)
         glDrawElements(GL_TRIANGLES, 6 * spriteCount, GL_UNSIGNED_INT, 0)
